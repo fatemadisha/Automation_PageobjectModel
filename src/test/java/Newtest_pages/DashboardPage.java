58b898c1baecba82484190b15b933abf0bb74d16
Kaniz Fatema <133844322+fatemadisha@users.noreply.github.com>
2023-07-18 00:25:21 +0600

    Dashboard commit with report

--- src/test/java/Newtest_pages/DashboardPage.java
+++ src/test/java/Newtest_pages/DashboardPage.java
@@ -1,18 +1,27 @@
 package Newtest_pages;
 
+import java.io.IOException;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
+
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+
 import Newtest_driver.PageDriver;
 import Newtest_utilities.Commonmethods;
-
+import Newtest_utilities.GetScreenshot;
 import junit.framework.Assert;
 
 public class DashboardPage extends Commonmethods{
 	
-	public DashboardPage() {
+	ExtentTest test;
+	
+	public DashboardPage(ExtentTest test) {
 		PageFactory.initElements(PageDriver.getCurrentDriver(), this);
+		this.test = test;
 	}
 	
 	@FindBys({
@@ -26,7 +35,7 @@ public class DashboardPage extends Commonmethods{
 	WebElement dashboardText;
 	
 	
-	public void checkDashboard() {
+	public void checkDashboard() throws IOException {
 //		String expectedText = "Dashboard";
 //		String actualText = dashboardText.getText();
 //		Assert.assertEquals(actualText, expectedText);
@@ -35,20 +44,39 @@ public class DashboardPage extends Commonmethods{
 				Assert.assertEquals(dashboardText.getText(), "Dashboard");
 			}
 		} catch (Exception e) {
-			System.out.println(e);
+			test.fail("<p style=\"color:#FF5353; font-size:13px\"><b>Dash Board Text is not locateable.Please check the error message.</b></p>");
+			Throwable t = new InterruptedException("Exception");
+			test.fail(t);
+			@SuppressWarnings("unused")
+			String screenShotPath = GetScreenshot.capture(PageDriver.getCurrentDriver(), "DashBoardText");
+			String dest = System.getProperty("user.dir") + "\\screenshots\\" + "DashboardText.png";
+			test.fail(MediaEntityBuilder.createScreenCaptureFromPath(dest).build());
+			Assert.assertTrue(dashboardText.isDisplayed());
+			PageDriver.getCurrentDriver().quit();
 		}
 		
 	}
 	
-	public void clickAdmin() {
+	public void clickAdmin() throws IOException {
 		try {
 			if(adminOption.isDisplayed()) {
 				adminOption.click();
 				timeout(5000);
+				test.pass("<p style=\"color:#85BC63; font-size:13px\"><b>You have successfully Clicked.</b></p>");
+				String screenShotPath = GetScreenshot.capture(PageDriver.getCurrentDriver(), "adminOption_ok");
+				String dest = System.getProperty("user.dir") + "\\screenshots\\" + "adminOption_ok.png";
+				test.pass(MediaEntityBuilder.createScreenCaptureFromPath(dest).build());
 			}
 		} catch (Exception e) {
-			System.out.println(e);
-			// TODO: handle exception
+			test.fail("<p style=\"color:#FF5353; font-size:13px\"><b>Admin Option is not Clickable.Please check the error message.</b></p>");
+			Throwable t = new InterruptedException("Exception");
+			test.fail(t);
+			@SuppressWarnings("unused")
+			String screenShotPath = GetScreenshot.capture(PageDriver.getCurrentDriver(), "adminOption");
+			String dest = System.getProperty("user.dir") + "\\screenshots\\" + "adminOption.png";
+			test.fail(MediaEntityBuilder.createScreenCaptureFromPath(dest).build());
+			Assert.assertTrue(adminOption.isDisplayed());
+			PageDriver.getCurrentDriver().quit();
 		}
 	}
 	
